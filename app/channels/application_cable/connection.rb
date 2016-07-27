42c59101dfdcd06564885a8cf61ee8fc6aa2713c
Hernan Y.Ke <yuriqiao@gmail.com>
2016-07-26 21:31:17 -0400

    cable connection

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,19 @@
+
+module ApplicationCable
+    class Connection < ActionCable::Connection::Base
+        identified_by :connected_user
+        def connect
+            self.connected_user = find_verified_user
+        end
+        
+        protected
+        
+        def find_verified_user
+            if connected_user = User.find_by(id:cookies.signed[:user_id])
+                connected_user
+            else
+                reject_unauthorized_connection
+            end
+        end
+    end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     def create
         if user.try(:authenticate, params[:password])
             session[:current_user] = user.id
+            cookies.signed[:user_id]||=user.id
             redirect_to root_url
         else
             redirect_to login_url
@@ -12,10 +13,13 @@ class SessionsController < ApplicationController
     end
     
     def destroy
+        session[:current_user]=nil
+        cookies.signed[:user_id]||=nil
+        redirect_to login_url
     end
     
     private
     def user
-        @user = User.find_by_email(params[:email])
+        @user ||= User.find_by_email(params[:email])
     end
 end
